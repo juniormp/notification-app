4ed72342c05bd0d8f2201167421eb5d22e70129c
Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>
2023-01-31 18:12:44 -0300

    create message model

---
+++ src/model/Message.js
@@ -0,0 +1,9 @@
+class Message {
+  constructor(category, content, dateTime) {
+    this.category = category;
+    this.content = content;
+    this.dateTime = dateTime;
+  }
+}
+
+export default Message;
