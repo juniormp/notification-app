9469689805a884a0b1e5bd0dbfd96109ac3c35bf
Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>
2023-01-31 19:40:57 -0300

    add route to post messages

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import express from "express";
 import users from "./seed/users.js";
+import SendNotificationService from "./src/services/SendNotificationService.js";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/api/users", (req, res) => {
   res.json(users);
+});
+
+app.post("/api/message", (req, res) => {
+  const {category, message} = req.body;
+  const sendNotification = new SendNotificationService();
+  sendNotification.sendNotification(category, message);
+
+  res.status(201).send();
 });
 
 app.listen(3000, () => {
