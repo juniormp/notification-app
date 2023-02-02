381fc66c1e1f10846c201b03193dc839293516ce
Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>
2023-02-01 21:13:39 -0300

    add unit test for push notification strategy

---
+++ spec/services/PushNotificationStrategy.spec.js
@@ -0,0 +1,44 @@
+import Message from "../../src/model/Message.js";
+import jest from "jest-mock";
+import sentMessageData from "../../db/sentMessageData.js";
+import PushNotificationStrategy from "../../src/services/PushNotificationStrategy.js";
+
+describe("PushNotificationStrategy", () => {
+  let strategy;
+
+  beforeEach(() => {
+    strategy = new PushNotificationStrategy();
+  });
+
+  test("send method adds message to sentMessageData", () => {
+    // Arrange
+    Date.now = jest.fn(() => new Date(Date.UTC(2023, 2, 1)).valueOf());
+    const user = {name: "Mauricio Junior"};
+    const category = "FINANCE";
+    const message = "Investments";
+    const se = jest.spyOn(sentMessageData, "push");
+
+    // Perform
+    strategy.send(user, category, message);
+
+    // Assert
+    expect(se).toHaveBeenCalledWith(new Message(category, message, Date.now()));
+  });
+
+  test("log method logs message to console", () => {
+    // Arrange
+    const user = {name: "Mauricio Junior"};
+    const message = "Investments";
+    const consoleSpy = jest.spyOn(console, "log");
+
+    // Perform
+    strategy.log(user, message);
+
+    // Assert
+    expect(consoleSpy).toHaveBeenCalledWith(
+      `Push log: Message "${message}" sent to ${
+        user.name
+      } at ${new Date().toLocaleString()}`
+    );
+  });
+});
